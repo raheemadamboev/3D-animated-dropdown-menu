a7710c3199aec8bdc59474b3f440191abd8294cd
Raheem <raheemideadev@gmail.com>
2021-09-24 21:41:59 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/a3danimateddropdownmenu/MainActivity.kt
@@ -0,0 +1,15 @@
+package xyz.teamgravity.a3danimateddropdownmenu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
